a4edf2743e9997e65b1e096389f0038f9e6dbdaf
cjk <chacos@gmail.com>
2024-03-11 19:50:10 +0700

    NotFound View 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ManagerListView from '../views/Manager/ManagerListView.vue';
 import ManagerEditView from '../views/Manager/ManagerEditView.vue';
+import NotFoundView from '../views/Error/NotFoundView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,6 +36,12 @@ const router = createRouter({
 			path: '/manager/:mngrId/edit',
 			name: 'ManagerEdit',
 			component: ManagerEditView,
+			// props: route => ({ id: parseInt(route.params.id) }),
+		},
+		{
+			path: '/:pathMatch(.*)*',
+			name: 'NotFound',
+			component: NotFoundView,
 			// props: route => ({ id: parseInt(route.params.id) }),
 		},
 	],
---
+++ src/views/Error/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="text-center py-5">
+		<h1>404 Not Found</h1>
+		<div class="text-muted mt-3">요청한 페이지를 찾을 수 없습니다.</div>
+		<div class="mt-5">
+			<RouterLink to="/">
+				<button class="btn btn-primary btn-xl fs130">HOME</button>
+			</RouterLink>
+		</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
