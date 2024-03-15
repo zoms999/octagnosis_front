d979895f0ba716c64735b33d06f81e474abe052e
DESKTOP-4DEDOJQ\zoms007 <hongsamcool1@gmail.com>
2024-03-16 04:24:01 +0900

    2024.03.16 npm i bootstrap-icons 설치

--- src/views/Manager/ManagerListView.vue
+++ src/views/Manager/ManagerListView.vue
@@ -1,38 +1,86 @@
 <template>
-	<hr class="my-4" />
-	<button class="btn btn-outline-dark" type="button" @click="goCreate">
-		등록
-	</button>
-	<AppLoading v-if="loading" />
-
-	<AppError v-else-if="error" :message="error.message" />
-
-	<template v-else-if="!isExist">
-		<p class="text-center py-4 text-muted">No Results</p>
-	</template>
-	<template v-else>
-		<AppGrid :items="posts" col-class="col-12">
-			<template v-slot="{ item }">
-				<ManagerItem
-					:mngrId="item.mngrId"
-					:useYn="item.useYn"
-					:email="item.email"
-					:mngrNm="item.mngrNm"
-					:phone="item.phone"
-					:tel="item.tel"
-					:authPersn="item.authPersn"
-					:authRsltView="item.authRsltView"
-					:authAdmin="item.authAdmin"
-					:authLogView="item.authLogView"
-					:authStati="item.authStati"
-					:authBbs="item.authBbs"
-					:authOrg="item.authOrg"
-					:insDt="item.insDt"
-					@click="goPage(item.mngrId)"
-				></ManagerItem>
-			</template>
-		</AppGrid>
-	</template>
+	<div>
+		<hr class="my-4" />
+		<button class="btn btn-outline-dark" type="button" @click="goCreate">
+			등록
+		</button>
+		<AppLoading v-if="loading" />
+		<AppError v-else-if="error" :message="error.message" />
+		<template v-else-if="!isExist">
+			<p class="text-center py-4 text-muted">No Results</p>
+		</template>
+		<template v-else>
+			<div class="container-fluid">
+				<!-- Page Heading -->
+				<h1 class="h3 mb-2 text-gray-800">Tables</h1>
+				<!-- DataTales Example -->
+				<div class="card shadow mb-4">
+					<div class="card-header py-3">
+						<h6 class="m-0 font-weight-bold text-primary">매니저 관리</h6>
+					</div>
+					<div class="card-body">
+						<div class="table-responsive">
+							<table
+								class="table table-bordered"
+								id="dataTable"
+								width="100%"
+								cellspacing="0"
+							>
+								<thead>
+									<tr>
+										<th>mngrId</th>
+										<th>useYn (Toggle)</th>
+										<th>email</th>
+										<th>mngrNm</th>
+										<th>phone</th>
+										<th>tel</th>
+										<th>authPersn</th>
+										<th>authRsltView</th>
+										<th>authAdmin</th>
+										<th>authLogView</th>
+										<th>authStati</th>
+										<th>authBbs</th>
+										<th>authOrg</th>
+										<th>insDt</th>
+									</tr>
+								</thead>
+								<tfoot></tfoot>
+								<tbody>
+									<tr
+										v-for="(item, index) in posts"
+										:key="index"
+										@click="goPage(item.mngrId)"
+									>
+										<td>{{ item.mngrId }}</td>
+										<td>
+											<button
+												class="btn btn-sm btn-outline-dark"
+												@click.stop="toggleUseYn(item)"
+											>
+												{{ item.useYn }}
+											</button>
+										</td>
+										<td>{{ item.email }}</td>
+										<td>{{ item.mngrNm }}</td>
+										<td>{{ item.phone }}</td>
+										<td>{{ item.tel }}</td>
+										<td v-html="getIcon(item.authPersn)"></td>
+										<td v-html="getIcon(item.authRsltView)"></td>
+										<td v-html="getIcon(item.authAdmin)"></td>
+										<td v-html="getIcon(item.authLogView)"></td>
+										<td v-html="getIcon(item.authStati)"></td>
+										<td v-html="getIcon(item.authBbs)"></td>
+										<td v-html="getIcon(item.authOrg)"></td>
+										<td>{{ item.insDt }}</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</template>
+	</div>
 </template>
 
 <script setup>
@@ -41,7 +89,6 @@ import axios from 'axios';
 import { useRouter } from 'vue-router';
 import { computed } from 'vue';
 import { useAxios } from '@/hooks/useAxios';
-import ManagerItem from '@/components/manager/ManagerItem.vue';
 
 const goCreate = () => {
 	router.push({
@@ -66,13 +113,24 @@ const {
 } = useAxios('/api/managers', { params });
 const isExist = computed(() => posts.value && posts.value.length > 0);
 const goPage = mngrId => {
-	// router.push(`/manager/${mngrId}`);
 	router.push({
 		name: 'ManagerEdit',
 		params: {
 			mngrId,
 		},
 	});
+};
+
+const toggleUseYn = item => {
+	item.useYn = item.useYn === 'Y' ? 'N' : 'Y';
+	// Call API to update `useYn` in the backend if needed
+};
+
+const getIcon = authValue => {
+	if (authValue === 0) return '<i class="bi bi-x-square"></i>';
+	else if (authValue === 1) return '<i class="bi bi-eye-fill"></i>';
+	else if (authValue === 2) return '<i class="bi bi-pencil-square"></i>';
+	else return '';
 };
 </script>
 
