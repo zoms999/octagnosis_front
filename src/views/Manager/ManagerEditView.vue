e136ce3d556d5f922a05ea426f9f39eb8fe2bbef
DESKTOP-4DEDOJQ\zoms007 <hongsamcool1@gmail.com>
2024-03-23 14:30:58 +0900

    2024.03.23 비밀번호변경

--- src/views/Manager/ManagerEditView.vue
+++ src/views/Manager/ManagerEditView.vue
@@ -23,7 +23,9 @@
 			@submit.prevent="edit"
 		>
 			<template #actionsPw>
-				<button class="btn btn-primary" @click.prevent="changPw">변경</button>
+				<button class="btn btn-primary" @click.prevent="changPwModal">
+					변경
+				</button>
 			</template>
 			<template #actions>
 				<div></div>
@@ -58,11 +60,53 @@
 	<Teleport to="#modal">
 		<AppModal v-model="show" title="비밀번호변경">
 			<template #default>
-				<p>제목 :</p>
-				<p>내용 :</p>
-				<p>등록일 :</p>
+				<div class="mb-3 row">
+					<label for="inputPassword" class="col-sm-4 col-form-label"
+						>현재 비밀번호</label
+					>
+					<div class="col-sm-8">
+						<input
+							type="password"
+							class="form-control"
+							id="userpw"
+							v-model="userpw"
+						/>
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label for="inputPassword" class="col-sm-4 col-form-label"
+						>변경 비밀번호</label
+					>
+					<div class="col-sm-8">
+						<input
+							type="password"
+							class="form-control"
+							id="userpw1"
+							v-model="userpw1"
+						/>
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label for="inputPassword" class="col-sm-4 col-form-label"
+						>비밀번호 확인</label
+					>
+					<div class="col-sm-8">
+						<input
+							type="password"
+							class="form-control"
+							id="userpw2"
+							v-model="userpw2"
+						/>
+						<div v-if="!passwordsMatch" class="text-danger">
+							비밀번호가 일치하지 않습니다.
+						</div>
+					</div>
+				</div>
 			</template>
 			<template #actions>
+				<button type="button" class="btn btn-secondary" @click="savePw">
+					비밀번호 변경
+				</button>
 				<button type="button" class="btn btn-secondary" @click="closeModal">
 					닫기
 				</button>
@@ -78,8 +122,9 @@ import ManagerForm from '@/components/manager/ManagerForm.vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useAxios } from '@/hooks/useAxios';
 import { useAlert } from '@/composables/alert';
-const { vAlert, vSuccess } = useAlert();
+import axios from 'axios';
 
+const { vAlert, vSuccess } = useAlert();
 const router = useRouter();
 const route = useRoute();
 const mngrId = route.params.mngrId;
@@ -125,13 +170,47 @@ const edit = () => {
 };
 
 const show = ref(false);
+const userpw = ref();
+const userpw1 = ref();
+const userpw2 = ref();
 
-const changPw = () => {
+const changPwModal = () => {
 	show.value = true;
+	userpw.value = ''; // 현재 비밀번호 초기화
+	userpw1.value = ''; // 변경 비밀번호 초기화
+	userpw2.value = ''; // 비밀번호 확인 초기화
 };
+const savePw = () => {
+	if (userpw1.value !== userpw2.value) {
+		vAlert('비밀번호가 일치하지 않습니다.');
+	} else {
+		axios
+			.patch(`/api/managers/chg/${mngrId}`, {
+				currentPassword: userpw.value,
+				newPassword: userpw1.value,
+			})
+			.then(response => {
+				// 비밀번호가 성공적으로 업데이트되었을 때
+				const requestData = JSON.parse(response.config.data);
+				const newPw = requestData.newPassword;
+				console.log('비밀번호가 업데이트되었습니다:', newPw); // 응답 데이터를 로그로 출력 (선택사항)
+				form.value.pw = newPw;
+				closeModal();
+			})
+			.catch(error => {
+				// 오류 메시지 표시
+				vAlert('현재 비밀번호가 잘못되었습니다.');
+				console.error('비밀번호 업데이트 중 오류 발생:', error); // 오류를 로그로 출력 (선택사항)
+			});
+	}
+};
+
 const closeModal = () => {
 	show.value = false;
 };
+
+const passwordsMatch = computed(() => userpw1.value === userpw2.value);
+
 const goDetailPage = () =>
 	router.push({ name: 'ManagerEdit', params: { mngrId } });
 </script>
