85e58c40b8f465e140a844d0fdd7226836e3ef7e
DESKTOP-4DEDOJQ\zoms007 <hongsamcool1@gmail.com>
2024-03-31 23:22:27 +0900

    개인관리

--- src/views/Personal/PersonalListView.vue
+++ src/views/Personal/PersonalListView.vue
@@ -1,84 +1,175 @@
 <template>
-	<div>
-		<input v-model="searchQuery" placeholder="검색어를 입력하세요" />
-		<button @click="search">검색</button>
-
-		<!-- DataTales Example -->
-		<div class="card shadow mb-4">
-			<div class="card-header py-3">
-				<h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-			</div>
-			<div class="card-body">
-				<div class="table-responsive">
-					<table
-						class="table table-bordered"
-						id="dataTable"
-						width="100%"
-						cellspacing="0"
-					>
-						<thead>
-							<tr>
-								<th>Name</th>
-								<th>Name</th>
-								<th>Name</th>
-								<th>Name</th>
-								<th>Name</th>
-								<th>Name</th>
-							</tr>
-						</thead>
-
-						<tbody>
-							<tr v-for="item in items" :key="item.persnId">
-								<td>{{ item.persnNm }}</td>
-								<td>{{ item.birthYear }}</td>
-								<td>{{ item.birthMonth }}</td>
-								<td>{{ item.sex }}</td>
-								<td>2011/04/25</td>
-								<td>$320,800</td>
-							</tr>
-						</tbody>
-					</table>
+	<div class="TitPage">
+		<div>개인관리</div>
+		<div></div>
+	</div>
+	<div class="SrchBox">
+		<div class="row">
+			<div class="col-8"></div>
+			<div class="col-4 text-end">
+				<div class="input-group w100p">
+					<input
+						type="text"
+						class="form-control"
+						id="SrchStr"
+						placeholder="기관명/대표/담당자"
+					/>
+					<button class="btn btn-primary w80">검색</button>
 				</div>
 			</div>
 		</div>
-
-		<button @click="loadMore" v-if="totalItems > items.length">더 보기</button>
 	</div>
+	<div class="FunBox">
+		<div>Total : {{ totalCount }}</div>
+		<button class="btn btn-primary" @click="Go('PersonalCreate', {})">
+			추가
+		</button>
+	</div>
+
+	<AppLoading v-if="loading"></AppLoading>
+
+	<AppError v-else-if="error" :message="error.message"></AppError>
+
+	<template v-else>
+		<table
+			class="table table-bordered Tbl1"
+			id="dataTable"
+			width="100%"
+			cellspacing="0"
+		>
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>이름</th>
+					<th>휴대폰</th>
+					<th>등록자</th>
+					<th>만기일자</th>
+					<th>검사</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr
+					v-for="item in PersonalList"
+					:key="item.PersnId"
+					@click="Go('PersonalEdit', { id: item.persnId })"
+					class="Poit"
+				>
+					<td>{{ item.persnId }}</td>
+					<td>{{ item.persnNm }}</td>
+					<td>{{ item.phone }}</td>
+					<td>{{ item.insDt }}</td>
+					<td>{{ item.ExpirationDate }}</td>
+					<!-- 예시: 만기일자에 해당하는 데이터의 실제 속성 이름으로 수정 -->
+					<td>보기</td>
+				</tr>
+			</tbody>
+			<tfoot></tfoot>
+		</table>
+
+		<!-- <AppPagination
+			:CurPage="CurPage"
+			:PageCnt="PageCnt"
+			@Page="GetPersonalList"
+		></AppPagination> -->
+
+		<AppPaginationTest
+			:current-page="params.paging.page"
+			:page-count="pageCount"
+			@page="page => (params.paging.page = page)"
+		/>
+	</template>
+
+	<Teleport to="#modal">
+		<AppModal v-model="show" title="게시글">
+			<template #default>
+				<p>제목 :</p>
+				<p>내용 :</p>
+				<p>등록일 :</p>
+			</template>
+			<template #actions>
+				<button type="button" class="btn btn-secondary" @click="closeModal">
+					닫기
+				</button>
+			</template>
+		</AppModal>
+	</Teleport>
+	<button class="btn btn-primary" @click="openModal">모달오픈</button>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { useAxios } from '@/hooks/useAxios';
+import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAlert } from '@/hooks/useAlert';
 
-const items = ref([]);
-const totalItems = ref(0);
-const limit = 10;
-let start = 0;
-const searchQuery = ref('');
+const { vAlert, vSuccess } = useAlert();
 
-const fetchData = async () => {
-	try {
-		const response = await axios.post('api/personal/personalList', {
-			start,
-			limit,
-			searchQuery: searchQuery.value,
-		});
-		totalItems.value = response.data.PersonalTotCnt;
-		items.value = response.data.PersonalList;
-	} catch (error) {
-		console.error('Error fetching data:', error);
-	}
+//const props = defineProps();
+//const emit = defineEmits(['ShowView']);
+const router = useRouter();
+
+const Go = (nm, q) => {
+	//router.push({ name: nm, params: p });
+	router.push({ name: nm, query: q });
 };
 
-const loadMore = async () => {
-	start += limit;
-	await fetchData();
+// List	************************************************
+
+const params = ref({
+	persnId: null,
+	persnNm: '',
+	srchStr: 'Str',
+	paging: {
+		page: 1,
+		pageBlock: 1,
+		limit: 5,
+		sort: 'createdAt',
+		order: 'desc',
+	},
+});
+const PersonalList = ref([]);
+const totalCount = ref(0);
+const pageCount = computed(() =>
+	Math.ceil(totalCount.value / params.value.paging.limit),
+);
+
+// Axios	**********************************************
+const { response, data, error, loading, execute, execUrl, reqUrl } = useAxios(
+	'',
+	{
+		method: 'post',
+	},
+	{
+		immediate: false,
+		onSuccess: () => {
+			totalCount.value = data.value.PersonalTotCnt;
+			PersonalList.value = data.value.PersonalList;
+			console.log(totalCount.value);
+			console.log(PersonalList.value);
+		},
+		onError: err => {
+			vAlert(err.message);
+		},
+	},
+);
+
+const GetPersonalList = async () => {
+	//params.value.paging.page = CurPage.value;
+	// params.value.paging.limit = 2;
+	console.log('GetPersonalList');
+	execUrl('/api/personal/personalList', params.value);
 };
 
-const search = async () => {
-	start = 0;
-	await fetchData();
-};
+GetPersonalList();
 
-onMounted(fetchData);
+const show = ref(false);
+const openModal = () => {
+	show.value = true;
+};
+const closeModal = () => {
+	show.value = false;
+};
 </script>
+
+<style lang="scss" scoped></style>
+@/hooks/alert
