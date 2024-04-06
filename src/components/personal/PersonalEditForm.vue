421c53f82e122579bb788187229e2cbc954db79e
DESKTOP-4DEDOJQ\zoms007 <hongsamcool1@gmail.com>
2024-04-07 00:02:05 +0900

    개인관리

--- src/components/personal/PersonalEditForm.vue
+++ src/components/personal/PersonalEditForm.vue
@@ -47,25 +47,208 @@
 			<div class="col-1 lbl">가입일</div>
 			<div class="col-2">
 				<div class="input-group">
-					<input
-						type="text"
-						class="form-control"
-						v-model="personal.PersnInsDt"
-					/>
+					<input type="text" class="form-control" v-model="personal.RegDt" />
 				</div>
 			</div>
 			<div class="col-1 lbl">사용기한</div>
 			<div class="col-2">
 				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.ExpirDt" />
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-1 lbl">이름</div>
+			<div class="col-2">
+				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.AcuntId" />
+				</div>
+			</div>
+			<div class="col-1 lbl">생년월일</div>
+			<div class="col-2">
+				<div class="input-group"></div>
+			</div>
+			<div class="col-1 lbl">성별</div>
+			<div class="col-2">
+				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.RegDt" />
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-1 lbl">휴대전화</div>
+			<div class="col-2">
+				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.Phone" />
+				</div>
+			</div>
+			<div class="col-1 lbl">추가연락처</div>
+			<div class="col-2">
+				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.Tel" />
+				</div>
+			</div>
+			<div class="col-1 lbl">이메일</div>
+			<div class="col-2">
+				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.Email" />
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-1 lbl"><i></i>우편번호</div>
+			<div class="col-3">
+				<div class="input-group">
+					<input type="text" class="form-control" />
+					<button class="btn btn-primary IconBtn">
+						<span class="material-icons"> search </span>
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-1 lbl">도로명 주소</div>
+			<div class="col-5">
+				<input type="text" class="form-control" />
+			</div>
+			<div class="col-1 lbl">지번 주소</div>
+			<div class="col-5">
+				<input type="text" class="form-control" />
+			</div>
+			<div class="col-1 lbl">상세 주소</div>
+			<div class="col-5">
+				<input type="text" class="form-control" />
+			</div>
+			<div class="col-1 lbl">추가 주소</div>
+			<div class="col-5">
+				<input type="text" class="form-control" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-1 lbl">로그확인</div>
+			<div class="col-4">
+				<div class="input-group">
+					<button
+						class="btn btn-primary IconBtn me-2"
+						@click.prevent="connectionLogModal"
+					>
+						<div class="d-flex">
+							<span class="me-2">접속로그</span>
+							<span class="material-icons"> poll </span>
+						</div>
+					</button>
+					<button
+						class="btn btn-primary IconBtn"
+						@click.prevent="authChangeLogModal"
+					>
+						<div class="d-flex">
+							<span class="me-2">변경로그</span>
+							<span class="material-icons"> poll </span>
+						</div>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="Tit1">
+		<div>최종(현재) 학력 정보</div>
+		<div></div>
+	</div>
+	<div class="container ItemBox">
+		<div class="row">
+			<div class="col-1 lbl">학업군</div>
+			<div class="col-2">
+				<select name="Educt" id="Educt">
+					<option value="" hidden selected>학력</option>
+					<option value="">초등</option>
+					<option value="">중등</option>
+					<option value="">고등</option>
+					<option value="">2,3년제 대학</option>
+					<option value="">4년제 대학교</option>
+					<option value="">대학원 석사</option>
+					<option value="">대학원 박사</option>
+				</select>
+			</div>
+			<div class="col-1 lbl">학교명</div>
+			<div class="col-2">
+				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.ScholNm" />
+				</div>
+			</div>
+			<div class="col-1 lbl">전공</div>
+			<div class="col-2">
+				<div class="input-group">
 					<input
 						type="text"
 						class="form-control"
-						v-model="personal.PersnInsDt"
+						v-model="personal.ScholMajor"
+					/>
+				</div>
+			</div>
+			<div class="col-1 lbl">학년</div>
+			<div class="col-2">
+				<div class="input-group">
+					<input
+						type="text"
+						class="form-control"
+						v-model="personal.ScholGrade"
 					/>
 				</div>
 			</div>
 		</div>
 	</div>
+
+	<div class="Tit1">
+		<div>최종(현재) 직업 정보</div>
+		<div></div>
+	</div>
+	<div class="container ItemBox">
+		<div class="row">
+			<div class="col-1 lbl">직업군</div>
+			<div class="col-2">
+				<select name="Job" id="Job">
+					<option value="" hidden selected>기타</option>
+					<option value="">학생</option>
+					<option value="">회사원</option>
+					<option value="">전문직</option>
+					<option value="">사업가</option>
+					<option value="">공무원</option>
+					<option value="">주부</option>
+					<option value="">무직</option>
+				</select>
+			</div>
+			<div class="col-1 lbl">직장명</div>
+			<div class="col-2">
+				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.JobNm" />
+				</div>
+			</div>
+			<div class="col-1 lbl">하는 일</div>
+			<div class="col-4">
+				<div class="input-group">
+					<input type="text" class="form-control" v-model="personal.JobDuty" />
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<Teleport to="#modal">
+		<AppModal v-model="connectionLogShow" title="접속로그">
+			<template #default>
+				<p>제목 :</p>
+				<p>내용 :</p>
+				<p>등록일 :</p>
+			</template>
+			<template #actions>
+				<button type="button" class="btn btn-secondary" @click="closeModal">
+					닫기
+				</button>
+			</template>
+		</AppModal>
+	</Teleport>
 </template>
 
 <script setup>
@@ -82,6 +265,21 @@ const togglePasswordVisibility = () => {
 	showPassword.value = !showPassword.value;
 	passwordFieldType.value = showPassword.value ? 'text' : 'password';
 };
+
+const connectionLogShow = ref(false);
+const authChangeLogShow = ref(false);
+const connectionLogModal = () => {
+	connectionLogShow.value = true;
+};
+const authChangeLogModal = () => {
+	authChangeLogShow.value = true;
+};
+
+const closeModal = () => {
+	connectionLogShow.value = false;
+	authChangeLogShow.value = false;
+};
 </script>
 
 <style lang="scss" scoped></style>
+@/hooks/alert
--- src/views/Personal/PersonalEditView.vue
+++ src/views/Personal/PersonalEditView.vue
@@ -1,7 +1,69 @@
 <template>
 	<div>
-		<PersonalEditForm :personal="personal"> </PersonalEditForm>
+		<PersonalEditForm :personal="personal" @GoBack="GoBack">
+			<template #actionsPw>
+				<button class="btn btn-primary" @click.prevent="changPwModal">
+					변경
+				</button>
+			</template>
+		</PersonalEditForm>
 	</div>
+	<Teleport to="#modal">
+		<AppModal v-model="show" title="비밀번호변경">
+			<template #default>
+				<div class="mb-3 row">
+					<label for="inputPassword" class="col-sm-4 col-form-label"
+						>현재 비밀번호</label
+					>
+					<div class="col-sm-8">
+						<input
+							type="password"
+							class="form-control"
+							id="userpw"
+							v-model="userpw"
+						/>
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label for="inputPassword" class="col-sm-4 col-form-label"
+						>변경 비밀번호</label
+					>
+					<div class="col-sm-8">
+						<input
+							type="password"
+							class="form-control"
+							id="userpw1"
+							v-model="userpw1"
+						/>
+					</div>
+				</div>
+				<div class="mb-3 row">
+					<label for="inputPassword" class="col-sm-4 col-form-label"
+						>비밀번호 확인</label
+					>
+					<div class="col-sm-8">
+						<input
+							type="password"
+							class="form-control"
+							id="userpw2"
+							v-model="userpw2"
+						/>
+						<div v-if="!passwordsMatch" class="text-danger">
+							비밀번호가 일치하지 않습니다.
+						</div>
+					</div>
+				</div>
+			</template>
+			<template #actions>
+				<button type="button" class="btn btn-secondary" @click="savePw">
+					비밀번호 변경
+				</button>
+				<button type="button" class="btn btn-secondary" @click="closeModal">
+					닫기
+				</button>
+			</template>
+		</AppModal>
+	</Teleport>
 </template>
 
 <script setup>
@@ -39,6 +101,50 @@ const getPersnByPersnIdAndType = async () => {
 	}
 };
 
+const show = ref(false);
+const userpw = ref();
+const userpw1 = ref();
+const userpw2 = ref();
+
+const changPwModal = () => {
+	show.value = true;
+	userpw.value = ''; // 현재 비밀번호 초기화
+	userpw1.value = ''; // 변경 비밀번호 초기화
+	userpw2.value = ''; // 비밀번호 확인 초기화
+};
+
+const savePw = () => {
+	if (!userpw1.value) {
+		vAlert('변경할 비밀번호를 입력하세요.');
+	} else if (userpw1.value !== userpw2.value) {
+		vAlert('비밀번호가 일치하지 않습니다.');
+	} else {
+		axios
+			.patch(`/api/personal/chgpw/${persnId}`, {
+				currentPassword: userpw.value,
+				newPassword: userpw1.value,
+			})
+			.then(response => {
+				const requestData = JSON.parse(response.config.data);
+				const newPw = requestData.newPassword;
+				console.log('비밀번호가 업데이트되었습니다:', newPw); // 응답 데이터를 로그로 출력 (선택사항)
+				//form.value.pw = newPw;
+				closeModal();
+			})
+			.catch(error => {
+				// 오류 메시지 표시
+				vAlert('현재 비밀번호가 잘못되었습니다.');
+				console.error('비밀번호 업데이트 중 오류 발생:', error); // 오류를 로그로 출력 (선택사항)
+			});
+	}
+};
+
+const closeModal = () => {
+	show.value = false;
+};
+const GoBack = () => {
+	window.history.back();
+};
 getPersnByPersnIdAndType();
 </script>
 
